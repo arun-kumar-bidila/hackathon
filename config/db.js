fbadbb60c737936cd1b3da66abd7de1dbd0cc9d2
arun-kumar-bidila <arunkumarbidila@gmail.com>
2024-12-21 20:06:06 +0530

    added securities

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 const mongoose=require("mongoose");
+require("dotenv").config()
 
-const connection=mongoose.createConnection("mongodb+srv://arunkumarbidila:Arun5452@cluster0.ss1sn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").on("open",()=>{
+
+const connection=mongoose.createConnection(process.env.DB_URL).on("open",()=>{
     console.log("mongodb connected successfully");
 }).on("error",()=>{
     console.log("error occured in connection");
